71830aa172c7ec386ecd98ef7ea3a6aea0bd8106
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-10-31 15:46:46 -0200

    First upload version.

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-import wsgiref.handlers
-from google.appengine.ext import webapp
+#!/usr/bin/env python
+import urllib
+
+from google.appengine.ext import blobstore, webapp
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Meme iOS Backend appliction')
+        upload_url = blobstore.create_upload_url('/img/upload')
+        self.response.out.write('''
+        <html>
+            <head>
+                <title>Yahoo! Meme iOS Backend application</title>
+            </head>
+            <body>
+                <form action="%s" method="POST" enctype="multipart/form-data">
+                    Upload File: <input type="file" name="file"><br>
+                    <input type="submit" name="submit" value="Submit">
+                </form>
+            </body>
+        </html>
+        ''' % upload_url)
+        
 
-class UploadHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('TO DO')
+class ImgHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
+
+class ImgUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file')
+        blob_info = upload_files[0]
+        self.redirect('/img/%s' % blob_info.key())
 
 def main():
     application = webapp.WSGIApplication([
-                    ('/', MainHandler),
-                    ('/upload/.*', UploadHandler),
+                ('/', MainHandler),
+                ('/img/([^/]+)?', ImgHandler),
+                ('/img/upload', ImgUploadHandler),
             ], 
             debug=True)
-    wsgiref.handlers.CGIHandler().run(application)
+    run_wsgi_app(application)
 
 if __name__ == '__main__':
     main()
