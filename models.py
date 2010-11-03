7553c107a74329f1f38e206f4736ea333573bfb1
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-11-03 18:30:22 -0200

    Added Auth model.

--- models.py
+++ models.py
@@ -9,3 +9,7 @@ class Post(db.Model):
     username = db.StringProperty()
     url = db.StringProperty()
     date = db.DateTimeProperty()
+
+class Auth(db.Model):
+    app_name = db.StringProperty()
+    secret = db.StringProperty()
