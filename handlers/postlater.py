54315777cb79191da8231308a429e2d35b495437
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-11-02 22:37:49 -0200

    Read posts from post later db.

--- handlers/postlater.py
+++ handlers/postlater.py
@@ -1,9 +1,43 @@
+import datetime
+
+from django.utils import simplejson
 from google.appengine.ext import db, webapp
+
+from models import Post
 
 class PostLaterAddHandler(webapp.RequestHandler):
     def get(self):
-        pass
+        post = Post(
+            username=self.request.get('username'), 
+            url=self.request.get('url'), 
+            date=datetime.datetime.now()
+        )
+        post.put()
+        
+        self.response.out.write('''
+        <html>
+            <head>
+                <title>Yahoo! Meme iOS Backend application</title>
+            </head>
+            <body>
+                <script type="text/javascript" charset="utf-8">
+                    window.close();
+                </script>
+            </body>
+        </html>
+        ''')
         
 class PostLaterHandler(webapp.RequestHandler):
     def get(self, username):
-        pass
+        posts = Post.all().filter('username =', username).order('date')
+
+        posts_data = []
+        for post in posts:
+            posts_data.append({
+                'id': str(post.key()), 
+                'url': post.url, 
+                'date': post.date.isoformat(), 
+            })
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(simplejson.dumps(posts_data))
--- models.py
+++ models.py
@@ -4,3 +4,8 @@ class Image(db.Model):
     data = db.BlobProperty()
     content_type = db.StringProperty()
     date = db.DateTimeProperty()
+
+class Post(db.Model):
+    username = db.StringProperty()
+    url = db.StringProperty()
+    date = db.DateTimeProperty()
